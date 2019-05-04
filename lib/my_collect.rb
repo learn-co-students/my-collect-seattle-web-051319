1de7472234f0cc51a4a6049538c90468427fbf03
Bryna Dunnells <badunnells@gmail.com>
2019-05-04 19:27:27 +0000

    collecting the new array

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(array) # put argument(s) here
+   i = 0
+   new_collection = []
+   while i <array.length
+      new_collection << yield(array[i])
+     i +=1
 
-
+   end# code here
+   new_collection
+end
